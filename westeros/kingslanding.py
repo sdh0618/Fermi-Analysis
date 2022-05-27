af2f08f1ad262f389d0b1b6412503b4ec341f16a
Deheng Song <songdeheng@yukawa.kyoto-u.ac.jp>
2022-05-27 17:51:05 +0900

    a

--- westeros/kingslanding.py
+++ westeros/kingslanding.py
@@ -1,6 +1,39 @@
 import numpy as np
 from scipy.integrate import quad
 
+def angsep(lon1, lat1, lon2, lat2):
+    
+    '''
+    angsep(lon1, lat1, lon2, lat2)
+        
+    Angular separation between two points (angles in degrees)
+    
+      The angular separation is calculated using the Vincenty formula [1]_,
+      which is slighly more complex and computationally expensive than
+      some alternatives, but is stable at at all distances, including the
+      poles and antipodes.
+      [1] http://en.wikipedia.org/wiki/Great-circle_distance
+    '''
+    
+    deg2rad = np.pi/180
+    lon1 = lon1*deg2rad
+    lat1 = lat1*deg2rad
+    lon2 = lon2*deg2rad
+    lat2 = lat2*deg2rad
+
+    sdlon = np.sin(lon2 - lon1)
+    cdlon = np.cos(lon2 - lon1)
+    slat1 = np.sin(lat1)
+    slat2 = np.sin(lat2)
+    clat1 = np.cos(lat1)
+    clat2 = np.cos(lat2)
+
+    num1 = clat2 * sdlon
+    num2 = clat1 * slat2 - slat1 * clat2 * cdlon
+    denom = slat1 * slat2 + clat1 * clat2 * cdlon
+
+    return np.arctan2(np.sqrt(num1*num1 + num2*num2), denom)/deg2rad
+
 def flux_converter(e_ref, e_min, e_max, index, flux=None, eflux=None, dnde=None, e2dnde=None):
     
     if flux!=None and eflux==None and dnde==None and e2dnde==None:
